4a7fc4977c5834b03064a75e79dfaf2c70b232bd
poteevprog <poteev.1999@yandex.ru>
2023-06-20 23:35:23 +0300

    user.ру

---
+++ user.py
@@ -0,0 +1,7 @@
+if __name__ == '__main__':
+    a = input("What is your name? ")
+    b = input("How old are you? ")
+    c = input("Where are you? ")
+    print("This is {0}.".format(a))
+    print("It is {0}.".format(b))
+    print("(S)he live in {0}.".format(c))
