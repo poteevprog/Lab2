16c56e7105b3e3c79e99fed07ad2785c9e4ca82c
poteevprog <poteev.1999@yandex.ru>
2023-06-21 00:10:29 +0300

    numbers

---
+++ numbers.py
@@ -0,0 +1,8 @@
+if __name__ == '__main__':
+    print("Введите 4 числа: ")
+    a = int(input())
+    b = int(input())
+    c = int(input())
+    d = int(input())
+    f = (a + b) / (c + d)
+    print('%.3f' % f)
