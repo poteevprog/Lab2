cc8902bf9f4213cd92c5d46b0aec9960e4728bce
poteevprog <poteev.1999@yandex.ru>
2023-06-20 23:51:20 +0300

    arithmetic.ру

---
+++ arithmetic.py
@@ -0,0 +1,7 @@
+if __name__ == '__main__':
+    print("4 * 100 - 54 ??")
+    a = int(input("answer = "))
+    correct = 4 * 100 - 54
+    print("correct answer: " + str(correct))  # str() преобразует значение внутри в строку
+    print("your answer: " + str(a))
+4
